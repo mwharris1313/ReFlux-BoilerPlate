0f25ed168644fade2e67ec68dec483f52a001a89
Michael Harris <mwharris333@gmail.com>
2015-06-03 00:52:45 -0700

    (feat) Create redis DB key-value read/write test

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
+console.log('server.js Loading...');
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = 3000;
 
-app.use(express.static(__dirname + '/public'));
+var redis = require('redis');
+var client = redis.createClient();
 
-io.on('connection', function(socket){
+client.on('error', function (err){
+  console.log('ERROR:', err);
+});
+
+// set key/value in redis database 
+client.set("myKey", "myValue", function (err, reply){
+  if (err) {
+    throw error;
+  } else {
+    console.log('REPLY', reply);
+
+    // read key/value in redis database
+    client.get('myKey', function (err, data){
+      if (err) {
+        throw error;
+      } else {
+        console.log('DATA', data);
+      }
+    });
+
+  }
+});
+
+app.use(express.static(__dirname + '/public')); // express route for index.html
+
+// socket.io, check for connection, if ping from client 'pong' back to client
+io.on('connection', function (socket){
   console.log('User Connecting');
-  socket.on('ping', function(data){
+  socket.on('ping', function (data){
     console.log('DATA', data);
     socket.emit('pong', 'PONG')
   });
