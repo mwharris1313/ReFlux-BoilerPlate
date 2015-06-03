b9125f33fa8896230c6e6e586a28a86b3507e595
Michael Harris <mwharris333@gmail.com>
2015-06-03 00:29:18 -0700

    (feat) Create socket.io client/server communications test

--- public/js/main.jsx
+++ public/js/main.jsx
@@ -1,5 +1,11 @@
 console.log('main.jsx Loading...');
 
+var socket = io();
+socket.emit('ping', 'PING');
+socket.on('pong', function(data){
+  console.log('DATA', data);
+});
+
 var TestApp = React.createClass({
   render: function() {
     var message = 'HelloWorld';
--- server.js
+++ server.js
@@ -6,5 +6,13 @@ var port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function(socket){
+  console.log('User Connecting');
+  socket.on('ping', function(data){
+    console.log('DATA', data);
+    socket.emit('pong', 'PONG')
+  });
+});
+
 server.listen(port);
 console.log('Listening on %s', port);
